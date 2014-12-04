1f313395a430263c5089ec9b0c2f5bbc37b17545
SondaSengupta <sondasengupta@gmail.com>
2014-12-04 14:50:42 -0600

    Added hardcoded tasks to the ToDo App

--- js/main.js
+++ js/main.js
@@ -3,5 +3,22 @@
   angular.module('todoApp',[])
     .controller('ToDoController',function(){
       var vm = this;
+      vm.tasks = [
+      {
+        name: 'Learn Angular',
+        desc: 'If I could learn Angular that would be Grand',
+        due: 'Today'
+      },
+      {
+        name: 'Finish Tic Tac Toe',
+        desc: 'If I could just figure out Firebase that would be Grand',
+        due: 'Monday'
+      },
+      {
+        name: 'Get a job!',
+        desc: 'If I could just get a developer job that would be Grand',
+        due: '4 months'
+      }
+      ]
     });
 }());
